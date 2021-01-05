aa9831fe2130a7f01057bb44d65ab79fa08af82b
JohnDuq <jonathan.duque.ramos@gmail.com>
2021-01-04 21:13:27 -0500

    Ajuste de imports Spring Test

--- spring-api-reactor/src/test/java/com/udemy/springboot/api/rest/reactor/app/RouterFunctionConfigTest.java
+++ spring-api-reactor/src/test/java/com/udemy/springboot/api/rest/reactor/app/RouterFunctionConfigTest.java
@@ -12,15 +12,13 @@ import com.udemy.springboot.api.rest.reactor.app.service.IItemService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import reactor.core.publisher.Mono;
 
-@AutoConfigureWebTestClient
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RouterFunctionConfigTest {
 
     @Autowired
--- spring-api-reactor/src/test/java/com/udemy/springboot/api/rest/reactor/app/SpringApiReactorApplicationTest.java
+++ spring-api-reactor/src/test/java/com/udemy/springboot/api/rest/reactor/app/SpringApiReactorApplicationTest.java
@@ -12,13 +12,15 @@ import com.udemy.springboot.api.rest.reactor.app.service.IItemService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import reactor.core.publisher.Mono;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 public class SpringApiReactorApplicationTest {
 
     @Autowired
